8389e959e4936e22cc24e356d68e80f8fc517831
ESLsamu <samuel@familie-kopp.de>
2017-12-12 16:05:49 +0100

    7i6

--- src/Agent/DummyAgent.java
+++
@@ -1,5 +0,0 @@
-package Agent;
-
-public class DummyAgent {
-
-}
--- src/GameLogic/GameMode.java
+++
@@ -1,5 +0,0 @@
-package GameLogic;
-//enumeration for different game modes in our demo
-public enum GameMode {
-	BOT, OORDER, ROWCLEAR 
-}
