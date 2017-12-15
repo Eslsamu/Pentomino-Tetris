59a384be505fdba0c31a9156c0f383b250cf3bbf
Eslsamu <samuel@familie-kopp.de>
2017-12-15 08:57:28 +0100

    Update DummyAgent.java

--- src/Agent/DummyAgent.java
+++ src/Agent/DummyAgent.java
@@ -1,5 +1,50 @@
 package Agent;
 
-public class DummyAgent {
+import java.util.ArrayList;
 
+import GameLogic.Direction;
+import GameLogic.PetrisGame;
+
+/**
+ * 
+ * @author Samuel
+ *
+ */
+public class DummyAgent extends Agent implements Comparable<DummyAgent>{
+	
+	public int score;
+	
+	public DummyAgent(double[] g) {	
+		super(g);
+	}
+	
+	@Override 
+	//comment
+	public void makeMove(PetrisGame g) {
+		
+		game = (TrainEnvironment) g;
+		
+		ArrayList<int[][]> moveList = possibleMoves2(game.getGrid(), game.getFallingBlock().getCoordinates());
+		
+		if(!game.gameOverCheck() && moveList.size()!=0) {
+			//moves the block to the evaluated position
+			game.getFallingBlock().setCoordinates(bestMove(game.getGrid(),moveList,false,game.getNextBlock().getCoordinates())); 	
+			game.move(Direction.DOWN);
+		}	
+			
+	}
+
+	@Override
+	public int compareTo(DummyAgent other) {
+		return -1*Integer.valueOf(this.score).compareTo(other.score);
+	}
+	
+	public void setScore(int s) {
+	    	score = s;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
 }
